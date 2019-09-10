a449b0dfb07295cfc8b1df629f0496791ae8b4be
ryoya_ito <horusu.dqx@gmail.com>
2019-09-11 00:52:00 +0900

    create URLSessionProtocol

--- Sources/QiitaAPIKit/QiitaAPIKit.swift
+++ Sources/QiitaAPIKit/QiitaAPIKit.swift
@@ -1,2 +1,9 @@
+import Foundation
+
 public struct QiitaAPIKit {
+    let urlSession: URLSessionProtocol
+    
+    init(_ urlSession: URLSessionProtocol = URLSession.shared) {
+        self.urlSession = urlSession
+    }
 }
---
+++ Sources/QiitaAPIKit/URLSession+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  URLSession+Ex.swift
+//  QiitaAPIKit
+//
+//  Created by 伊藤凌也 on 2019/09/11.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+}
+
+extension URLSession: URLSessionProtocol {
+}

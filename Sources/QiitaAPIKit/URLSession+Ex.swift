baa82868faf93cad74fe0e1d4f7f067baafd575b
ryoya_ito <horusu.dqx@gmail.com>
2019-09-28 16:53:04 +0900

    apply URLSessionProtocol

--- Sources/QiitaAPIKit/URLSession+Ex.swift
+++ Sources/QiitaAPIKit/URLSession+Ex.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
 
 extension URLSession: URLSessionProtocol {

eb814e773ab83e17672447c682eb64332eaa7998
ryoya_ito <horusu.dqx@gmail.com>
2019-10-01 14:23:44 +0900

    make example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 import QiitaAPIKit
 
-class ViewController: UITableViewController {
+protocol ViewProtocol: AnyObject {
+    func reloadData()
+    func showErrorMessage()
+}
 
-    var items: [Article] = []
+class ViewController: UIViewController, ViewProtocol {
+
+    private(set) var presenter: ViewPresenter!
+
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.presenter = ViewPresenter(self)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(nibName: nil, bundle: nil)
+        self.presenter = ViewPresenter(self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.items.count
+    }
 
-        cell.textLabel?.text = items[indexPath.row].body
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = presenter.items[indexPath.row]
+
+        cell.textLabel?.text = item.title
+
+        return cell
+    }
+
+    func reloadData() {
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+
+    func showErrorMessage() {
+        let alert = UIAlertController(title: "Error", message: "any error occured", preferredStyle: .alert)
+        present(alert, animated: true)
     }
 }

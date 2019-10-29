f74331a359e7e417694f5a380581cccc0c05892e
ryoya ito <horusu.dqx@gmail.com>
2019-10-29 15:04:25 +0900

    :recycle: use common protocol for request

---
+++ Sources/QiitaAPIKit/RequestType.swift
@@ -0,0 +1,50 @@
+//
+//  RequestType.swift
+//  QiitaAPIKit
+//
+//  Created by 伊藤凌也 on 2019/10/29.
+//
+
+import Foundation
+
+protocol RequestType {
+    associatedtype Response: Decodable
+
+    var requestURLString: String { get }
+    var queryItems: [URLQueryItem]? { get }
+
+    func request(completion: @escaping (Result<Response, Error>) -> Void)
+}
+
+extension RequestType {
+    func request(completion: @escaping (Result<Response, Error>) -> Void) {
+        var components = URLComponents(string: requestURLString)!
+        components.queryItems = queryItems ?? []
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        var request = URLRequest(url: components.url!)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(APIError.DataIsNotFetched))
+                return
+            }
+            do {
+                let response = try decoder.decode(Response.self, from: data)
+                completion(.success(response))
+            } catch let e {
+                completion(.failure(e))
+            }
+        }
+        task.resume()
+    }
+}

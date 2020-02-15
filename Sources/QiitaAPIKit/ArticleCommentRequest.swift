1b92837c976fbf499584538aa635d77a6af6be87
ryoya ito <horusu.dqx@gmail.com>
2020-02-15 15:15:04 +0900

    :recycle: rm root type `QiitaAPIKit`

--- Sources/QiitaAPIKit/ArticleCommentRequest.swift
+++ Sources/QiitaAPIKit/ArticleCommentRequest.swift
@@ -7,34 +7,31 @@
 
 import Foundation
 
-public extension QiitaAPIKit {
+/// QiitaAPI v2 comments API
+/// https://qiita.com/api/v2/docs#get-apiv2itemsitem_idcomments
+public struct ArticleCommentRequest: RequestType {
+    public typealias Request = ArticleComment.Request
+    public typealias Response = [ArticleComment.Response]
 
-    /// QiitaAPI v2 comments API
-    /// https://qiita.com/api/v2/docs#get-apiv2itemsitem_idcomments
-    struct ArticleCommentRequest: RequestType {
-        public typealias Request = ArticleComment.Request
-        public typealias Response = [ArticleComment.Response]
+    /// Article ID
+    let id: String
 
-        /// Article ID
-        let id: String
+    public var requestURLString: String {
+        return "https://qiita.com/api/v2/items/\(id)/comments"
+    }
+    public var requestQueryItem: ArticleComment.Request {
+        return ArticleComment.Request()
+    }
 
-        public var requestURLString: String {
-            return "https://qiita.com/api/v2/items/\(id)/comments"
-        }
-        public var requestQueryItem: ArticleComment.Request {
-            return ArticleComment.Request()
-        }
+    public var httpMethod: HttpMethod = .GET
 
-        public var httpMethod: HttpMethod = .GET
+    public var accessToken: String?
 
-        public var accessToken: String?
-
-        /// initializer
-        /// - Parameter accessToken: Qiita access token
-        /// - Parameter id: Article ID
-        public init(accessToken: String? = nil, id: String) {
-            self.accessToken = accessToken
-            self.id = id
-        }
+    /// initializer
+    /// - Parameter accessToken: Qiita access token
+    /// - Parameter id: Article ID
+    public init(accessToken: String? = nil, id: String) {
+        self.accessToken = accessToken
+        self.id = id
     }
 }
--- Sources/QiitaAPIKit/ArticleLikesRequest.swift
+++ Sources/QiitaAPIKit/ArticleLikesRequest.swift
@@ -7,34 +7,31 @@
 
 import Foundation
 
-public extension QiitaAPIKit {
+/// QiitaAPI v2 likes API
+/// https://qiita.com/api/v2/docs#get-apiv2itemsitem_idlikess
+public struct ArticleLikesRequest: RequestType {
+    public typealias Request = ArticleLike.Request
+    public typealias Response = [ArticleLike.Response]
 
-    /// QiitaAPI v2 likes API
-    /// https://qiita.com/api/v2/docs#get-apiv2itemsitem_idlikess
-    struct ArticleLikesRequest: RequestType {
-        public typealias Request = ArticleLike.Request
-        public typealias Response = [ArticleLike.Response]
+    /// Article ID
+    let id: String
 
-        /// Article ID
-        let id: String
+    public var requestURLString: String {
+        return "https://qiita.com/api/v2/items/\(id)/likes"
+    }
+    public var requestQueryItem: ArticleLike.Request {
+        return ArticleLike.Request()
+    }
 
-        public var requestURLString: String {
-            return "https://qiita.com/api/v2/items/\(id)/likes"
-        }
-        public var requestQueryItem: ArticleLike.Request {
-            return ArticleLike.Request()
-        }
+    public var httpMethod: HttpMethod = .GET
 
-        public var httpMethod: HttpMethod = .GET
+    public var accessToken: String?
 
-        public var accessToken: String?
-
-        /// initializer
-        /// - Parameter accessToken: Qiita access token
-        /// - Parameter id: Article ID
-        public init(accessToken: String? = nil, id: String) {
-            self.accessToken = accessToken
-            self.id = id
-        }
+    /// initializer
+    /// - Parameter accessToken: Qiita access token
+    /// - Parameter id: Article ID
+    public init(accessToken: String? = nil, id: String) {
+        self.accessToken = accessToken
+        self.id = id
     }
 }
